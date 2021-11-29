96fd738817ac4bff553e6f4b1304a7a55e23ae13
ramirolynch <ramirolynch@yahoo.com>
2021-11-28 21:59:08 -0600

    removed database arrays from script.js

--- script.js
+++ script.js
@@ -1,60 +1,13 @@
 
 (function(){
 
-    const allPotions = [
-        {
-            name: 'Potion 1',
-            price:4
-        },
-        {
-            name: 'Potion 2',
-            price: 3
-
-        },
-        {
-            name: 'Potion 3',
-            price:5
-        },
-        {
-            name: 'Potion 4',
-            price: 5
-
-        },
-        {
-            name: 'Potion 5',
-            price:4
-        },
-        {
-            name: 'Potion 6',
-            price: 3
-
-        },
-        {
-            name: 'Potion 7',
-            price:5
-        },
-        {
-            name: 'Potion 8',
-            price: 5
-
-        },
-        {
-            name: 'Potion 9',
-            price:5
-        },
-        {
-            name: 'Potion 10',
-            price: 5
-
-        },
-        ]
 
     const allPotionsItems = document.querySelectorAll('.potions')
 
 
     for (let i=0; i<allPotions.length; i++) {
     
-        allPotionsItems[i].innerText = `${allPotions[i].name} ${allPotions[i].price}`
+        allPotionsItems[i].innerText = `${allPotions[i].name} $${allPotions[i].price}`
     }
 
 
