3b1d84e24809c9ee3f604b758e7d8bac1ec4f84b
ramirolynch <ramirolynch@yahoo.com>
2021-11-29 23:02:24 -0600

    made changes

--- script.js
+++ script.js
@@ -4,43 +4,19 @@
     const allPotionsItems = document.querySelectorAll('.potions')
 
 
-    for (let i=0; i<allPotions.length; i++) {
-    
-        allPotionsItems[i].innerText = `${allPotions[i].name} (${allPotions[i].category}) $${allPotions[i].price}`
-    }
+// lists all of the products in the initial page
+for (let i=0; i<allPotions.length; i++) {
+
+    allPotionsItems[i].innerText = `${allPotions[i].name} (${allPotions[i].category}) $${allPotions[i].price}`
+}
 
 
-//    const cartArea = document.querySelector('#cartList')
-   
-//    let total = 0;
 
-//     document.body.addEventListener('click', event => {
-
-//         const listItem = document.createElement('li')
-        
-//         const id = event.target.id
-//         listItem.innerText = `${allPotions[id].name} $${allPotions[id].price}`
-//         total += parseInt(allPotions[id].price)
-
-//         cartArea.append(listItem)
-         
-//         if (document.querySelector('#totalCart')) {
-//             document.querySelector('#totalCart').remove()
-//         }
-
-//         let totalCart = document.createElement('li')
-//         totalCart.setAttribute('id', 'totalCart')
-//         totalCart.innerText = `Total: $${total}`
-
-//         cartArea.append(totalCart)   
-        
-//     })
-
-
-    const hotButton = document.querySelector('nav>ul>li:nth-child(1)');
-    const coldButton = document.querySelector('nav>ul>li:nth-child(2)');
-
-    
+// getting category buttons
+const hotButton = document.querySelector('nav>ul>li:nth-child(1)');
+const coldButton = document.querySelector('nav>ul>li:nth-child(2)');
+  
+    // hot button category functionality
     hotButton.addEventListener('click', event => {
 
         if (event.target.id === 'hot' && event.target.style.backgroundColor !== 'black') {
@@ -67,6 +43,7 @@
 
 })
 
+// cold button category functionality
 coldButton.addEventListener('click', event => {
 
     if (event.target.id === 'cold') {
@@ -88,11 +65,50 @@ coldButton.addEventListener('click', event => {
         }
 
     }
-
 }
 
 })
 
+// this is the pop up functionality
+
+    const productSection = document.querySelector('#productSection')
+    const cartAside = document.querySelector('#cartAside')
+
+    const cartArea = document.querySelector('#cartList')
+
+    const footer = document.querySelector('footer')
+    const footerButton = document.querySelector('footer>button')
+   
+    let total = 0;
+
+    productSection.addEventListener('click', event => {
+
+        productSection.style.display = 'none';
+        cartAside.style.display = 'block';
+        footerButton.innerText = 'Add to Cart'
+
+        const listItem = document.createElement('li')
+        
+        const id = event.target.id
+        listItem.innerText = `${allPotions[id].name} (${allPotions[id].category}) $${allPotions[id].price}`
+        listItem.classList.add('addToCart')
+        total += parseInt(allPotions[id].price)
+
+        cartArea.append(listItem)
+         
+        if (document.querySelector('#totalCart')) {
+            document.querySelector('#totalCart').remove()
+        }
+
+        let totalCart = document.createElement('span')
+        totalCart.setAttribute('id', 'totalCart')
+        totalCart.style.color = 'white'
+        totalCart.style.marginRight = '1.5em'
+        totalCart.innerText = `Total: $${total}`
+
+        footer.append(totalCart)   
+        
+    })
 
 
 })();
