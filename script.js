31d0993bd6b9962c5833d741441ea329ad39fe10
ramirolynch <ramirolynch@yahoo.com>
2021-11-29 21:39:59 -0600

    made changes to index.html script.js and styles.js

--- script.js
+++ script.js
@@ -1,45 +1,97 @@
 
 (function(){
 
-
     const allPotionsItems = document.querySelectorAll('.potions')
 
 
     for (let i=0; i<allPotions.length; i++) {
     
-        allPotionsItems[i].innerText = `${allPotions[i].name} $${allPotions[i].price}`
+        allPotionsItems[i].innerText = `${allPotions[i].name} (${allPotions[i].category}) $${allPotions[i].price}`
     }
 
 
-   const cartArea = document.querySelector('#cartList')
+//    const cartArea = document.querySelector('#cartList')
    
-   let total = 0;
+//    let total = 0;
 
-    document.body.addEventListener('click', event => {
+//     document.body.addEventListener('click', event => {
 
-        const listItem = document.createElement('li')
+//         const listItem = document.createElement('li')
         
-        const id = event.target.id
-        listItem.innerText = `${allPotions[id].name} $${allPotions[id].price}`
-        total += parseInt(allPotions[id].price)
+//         const id = event.target.id
+//         listItem.innerText = `${allPotions[id].name} $${allPotions[id].price}`
+//         total += parseInt(allPotions[id].price)
 
-        cartArea.append(listItem)
+//         cartArea.append(listItem)
          
-        if (document.querySelector('#totalCart')) {
-            document.querySelector('#totalCart').remove()
+//         if (document.querySelector('#totalCart')) {
+//             document.querySelector('#totalCart').remove()
+//         }
+
+//         let totalCart = document.createElement('li')
+//         totalCart.setAttribute('id', 'totalCart')
+//         totalCart.innerText = `Total: $${total}`
+
+//         cartArea.append(totalCart)   
+        
+//     })
+
+
+    const hotButton = document.querySelector('nav>ul>li:nth-child(1)');
+    const coldButton = document.querySelector('nav>ul>li:nth-child(2)');
+
+    
+    hotButton.addEventListener('click', event => {
+
+        if (event.target.id === 'hot' && event.target.style.backgroundColor !== 'black') {
+
+
+            hotButton.style.backgroundColor = 'black';
+            coldButton.style.backgroundColor = 'blueviolet';
+
+            for (let i=0; i<allPotions.length; i++) {
+
+                if(allPotions[i].category === 'Hot') {
+    
+                allPotionsItems[i].style.display = 'flex';
+
+            }
+            else {
+
+                    allPotionsItems[i].style.display = 'none';
+                
+            }   
         }
 
-        let totalCart = document.createElement('li')
-        totalCart.setAttribute('id', 'totalCart')
-        totalCart.innerText = `Total: $${total}`
+    }
 
-        cartArea.append(totalCart)   
-        
+})
 
+coldButton.addEventListener('click', event => {
 
-        
+    if (event.target.id === 'cold') {
 
-    })
+        hotButton.style.backgroundColor = 'blueviolet';
+        coldButton.style.backgroundColor = 'black';
+
+        for (let i=0; i<allPotions.length; i++) {
+
+            if(allPotions[i].category === 'Cold') {
+
+            allPotionsItems[i].style.display = 'flex';
+
+        }
+        else {
+
+            allPotionsItems[i].style.display = 'none';
+
+        }
+
+    }
+
+}
+
+})
 
 
 
