f7cadd742821407d1dc194f187c59e05b55a7676
CD Rivers <riverscd90@gmail.com>
2021-11-29 21:34:24 -0500

    added payment js file and updated payment html

---
+++ payment.js
@@ -0,0 +1,18 @@
+(function () {
+   
+const cardPayment = document.querySelector("#card")
+const cashPayment = document.querySelector("#cash")
+const cardForm = document.querySelector("#card-information")
+
+
+
+
+
+
+
+
+
+
+
+  })();
+  
