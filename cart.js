7cbfa0dd3276f02cb6ee1082ad221e2874c6047e
IamCoy <jacoyiaj@yahoo.com>
2021-12-02 01:04:17 -0500

    added products now displaying in cart

--- cart.js
+++ cart.js
@@ -1,7 +1,58 @@
 (function(){
+    let list = document.querySelector("ul")
+    let cartProducts = JSON.parse(localStorage.getItem("cartContent"));
+    let cartArray = cartProducts.addedProducts
+    let cartTable = document.querySelector("main")
+    
+    
+    
+     cartArray.forEach((element) => {
+        console.log(element)
+        
+            let tbl = document.createElement("table");
+            tbl.style.width = "100%"
+        
+            let tblBody = document.createElement("tbody");
+          
+            // for (let r = 0; r < cartArray.length; r++) {
+             
+              let row = document.createElement("tr")
+              
+            
+          
+            //   for (let j = 0; j < cartArray.length; j++) {
+              
+                let cell = document.createElement("td");
+                cell.style.height = "50px"
+                cell.innerText= `${element.name} ${element.quanity} $${element.price}  `
+                cell.setAttribute("class" , "cart-tbl")
+                row.appendChild(cell);
+                tblBody.appendChild(row);
+                tbl.appendChild(tblBody);
+                cartTable.appendChild(tbl)
+
+              })
+
+              
+
+        // productsInCart.innerText = cartArray[i].name
+       
+
+        // let editButton = document.createElement("button")
+        // let removeButton = document.createElement("button")
+
+        // editButton.innerText = "edit"
+        // removeButton.innerText = "remove"
+
+        // list.append(productsInCart)
+        // console.log("hello")
+     
+
+
 
     
 
+  
 
 
 
