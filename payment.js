d6c5d7b89838b1eea8bc9f9b32c1bbd5422ea3ff
CD Rivers <riverscd90@gmail.com>
2021-12-03 14:19:17 -0500

    added merriweather font family to payment and ordercomplete pages

--- payment.js
+++ payment.js
@@ -15,6 +15,49 @@ card.addEventListener('click', function(){
   cardInfoForm.style.display = ""
 });
 
+
+let customerCart = document.querySelector("section > ul");
+
+let cartProducts = JSON.parse(localStorage.getItem("cartContent"));
+let cartArray = cartProducts.addedProducts;
+let summaryTotal = document.querySelector("section > span")
+let summarySection = document.querySelector("section")
+
+let checkoutTotal = 0;
+
+for (let i=0; i<cartArray.length; i++) {
+
+  let cartItems = document.createElement("li");
+  cartItems.innerText = `${cartArray[i].name} $${cartArray[i].price * cartArray[i].quantity}`;
+  customerCart.append(cartItems);
+
+  checkoutTotal += cartArray[i].price * cartArray[i].quantity;
+  summaryTotal.innerText = `Total $${checkoutTotal}`
+  summarySection.append(summaryTotal)
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // see if i can change click listener to inputs instead
 
 // set payNow button to go to verifcation if accepted
