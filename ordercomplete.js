df402ac8256abbbf0d46d05beb2c7fd7371740d7
CD Rivers <riverscd90@gmail.com>
2021-12-03 20:53:16 -0500

    cash form adjustments including hiding arrows and changing change back template

--- ordercomplete.js
+++ ordercomplete.js
@@ -1,11 +1,34 @@
 (function () {
    
-    const thankYou= document.querySelector("span")
-    const customerName = document.querySelector("#first-name")
+    //const thankYou= document.querySelector("span")
+    //const customerName = document.querySelector("#first-name")
     
-    let userName = customerName.value
-    thankYou.innerText = `${userName}`
+    // let userName = customerName.value
+    // thankYou.innerText = `${userName}`
 
     //look into session storage on local storage
+
+  let customerCart = document.querySelector("section > ul");
+
+  let cartProducts = JSON.parse(localStorage.getItem("cartContent"));
+  let cartArray = cartProducts.addedProducts;
+  let summaryTotal = document.querySelector("section > span")
+  let summarySection = document.querySelector("section")
+
+  let checkoutTotal = 0;
+
+  console.log(cartProducts)
+
+  for (let i=0; i<cartArray.length; i++) {
+
+    let cartItems = document.createElement("li");
+    cartItems.innerText = `${cartArray[i].name} $${cartArray[i].price * cartArray[i].quantity}`;
+    customerCart.append(cartItems);
+
+    checkoutTotal += cartArray[i].price * cartArray[i].quantity;
+    summaryTotal.innerText = `Total $${checkoutTotal}`
+    summarySection.append(summaryTotal)
+
+  };
     
-      })();
+})();
