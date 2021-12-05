435bea963016e3ebc745925797a7aa220e2aaad2
IamCoy <jacoyiaj@yahoo.com>
2021-12-04 19:37:33 -0500

    debugging

--- cart.js
+++ cart.js
@@ -10,12 +10,6 @@
     const cartTax = document.getElementById("tax")
     const cartTotalFooter = document.getElementById("cartTotal")
     
-    // console.log(cartArray)
-    // for (var x in cartArray) {
-    //   console.log(x + ": " + cartArray[x])
-  // }
-    
- 
 
 
     
@@ -32,25 +26,26 @@
               removeButton.style.backgroundColor = "transparent"
               removeButton.style.border = "none"
               removeButton.style.color = "blueviolet"
+              removeButton.style.lineHeight = "3"
               removeButton.setAttribute("id" , element.idNumber)
               
           
-             
-              
                 let cell1 = document.createElement("td");
                 let cell2 = document.createElement("td");
                 let cell3 = document.createElement("td");
+
+
                 cell1.style.height = "50px"
                 cell1.innerText= `${element.quantity}`
-                cell1.setAttribute("class" , "cart-tbl")
+               
 
                 cell2.style.height = "50px"
                 cell2.innerText= `${element.name} `
-                cell2.setAttribute("class" , "cart-tbl") 
+                
 
                 cell3.style.height = "50px"
                 cell3.innerText= ` $${element.price}`
-                cell3.setAttribute("class" , "cart-tbl")
+               
                
 
 
@@ -66,20 +61,6 @@
                 row.appendChild(removeButton)
                 
 
-              //  console.log(element)
-                // let cartTotal = 0
-              
-                // cartSubtotal.innerText = `$${element.price * element.quantity}`;
-              
-              
-               
-                 
-                
-                
-              
-
-                
-            
 
               })
 
@@ -112,39 +93,14 @@
                
               }
 
-              cartSubtotal.innerText = tempSubtotal
+              cartSubtotal.innerText = tempSubtotal + ".00"
 
-              cartTax.innerText =  parseFloat(cartSubtotal.innerText * 0.06)
+              cartTax.innerText = "$" + parseFloat(cartSubtotal.innerText * 0.06)
               
               cartTotal += parseFloat(cartSubtotal.innerText * 0.06) +tempSubtotal
-              cartTotalFooter.innerText = cartTotal
 
-
-
-    
-
-             
-
-              
-
-        // productsInCart.innerText = cartArray[i].name
+              cartTotalFooter.innerText = `$${cartTotal}`
        
-
-        
-
-       
-     
-
-
-
-    
-
-  
-
-
-
-
-
 
 
 })();
